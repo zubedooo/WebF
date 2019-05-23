7e5105477b9ab7823032576e0b538d80a11d2f88
zubedooo <34791155+zubedooo@users.noreply.github.com>
2019-05-23 23:33:58 +0530

    Update 10a.js

--- 10a.js
+++ 10a.js
@@ -1,16 +1,24 @@
-const express=require('express')
-const app=express();
-var count=0;
-var logger=(req,res,next)=>{
-    req.count=count++;
-    console.log(`Logged`);
-    next();
+//node 10a.js
+//Go to browser. Type in localhost:3000 
+//refresh to get count increasing
+//
+var express = require('express')
+const app = express()
+var count = 0
+
+var Logger = (req, res, next)=>{
+    req.count = count++
+    console.log(count)
+    next()
 }
-var visited=(req,res,next)=>{
-    console.log(`No of the time visited: ${req.count}`)
-    next();
+var visited = (req, res, next)=>{
+    console.log(`No of times visited: , ${req.count}`)
+    next()
 }
-app.get('/',[logger,visited],(req,res)=>{
-    res.send("Visited : "+req.count);
+app.use(Logger)
+app.use(visited)
+app.get('/', (req, res)=>{
+    res.send("visited: "+req.count)
 })
-app.listen(3000);
+
+app.listen(3000)
