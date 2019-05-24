4bc8442f332632cc38b703fd8cdff5a8d3b61459
zubedooo <34791155+zubedooo@users.noreply.github.com>
2019-05-24 13:27:59 +0530

    Update 1b.js

--- 1b.js
+++ 1b.js
@@ -28,3 +28,20 @@ mongoClient.connect('mongodb://127.0.0.1/students', function(err, db) {
         app.listen(3000);
     }
 });
+
+//1b.html
+<!DOCTYPE html>
+<html>
+    <head>
+        Student Database
+    </head>
+    <body>
+        <form action="insert" method="GET">
+            USN: <input type="text" name="usn" /><br/>
+            Name: <input type="text" name="name" /><br/>
+            Subject Code: <input type="text" name="code" /><br/>
+            CIE Marks: <input type="text" name="marks" /><br/>
+            <input type="submit" name="Submit" />
+        </form>
+    </body>
+</html>
