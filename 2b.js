308bc210183b66d828867496098f184d5d5d001c
zubedooo <34791155+zubedooo@users.noreply.github.com>
2019-05-25 21:51:35 +0530

    Update 2b.js

--- 2b.js
+++ 2b.js
@@ -1,49 +1,46 @@
-var express = require('express');  
-var app = express();  
-var expressValidator=require('express-validator');
-var bodyParser=require('body-parser');
-var uep=bodyParser.urlencoded({extended: false});
-app.use(express.static('public'));  
+var express = require('express');
+var app = express();
+var expressValidator = require('express-validator');
+var bodyParser = require('body-parser');
+var urlencodedparser = bodyParser.urlencoded({extended: false});
+
+app.use(express.static('public'));
 app.use(expressValidator());
-app.get('/2b.html', function(req,res)
-{
-res.sendFile(__dirname+'/'+'2b.html');
+app.get('/2b.html', function(req, res) {
+    res.sendFile(__dirname+'/'+'2b.html');
+});
+app.post('/insert', urlencodedparser, function(req, res) {
+    req.checkBody('name', 'Enter name').notEmpty();
+    req.checkBody('surname', 'Surname not entered properly').isString();
+    req.checkBody('salary', 'Salary must be integer').isInt();
+
+    var errors = req.validationErrors();
+    if(errors){
+        console.log(errors);
+        res.send(errors);
+    }
+    else{
+        console.log(JSON.stringify(req.body));
+        res.end(JSON.stringify(req.body));
+    }
 });
 
-app.post('/insert',uep,function(req,res)
-{
-req.checkBody('namee','Should NOT be empty').notEmpty();
-req.checkBody('marks','Should be number').isInt();
-var errors=req.validationErrors();
-
-if(errors)
-{
-	console.log(errors);
-	res.send(errors);
-}
-
-else
-{
-console.log(JSON.stringify(req.body));
-res.end(JSON.stringify(req.body));
-}
-
-});
-
-app.listen(4005);
+app.listen(3000);
 
 //2b.html
+<!DOCTYPE html>
 <html>
-<head>
-<title>Student detail</title>
-</head>
-<body>
-<form action="insert" method="post">
-USN <input type="text" id="usn" name="usn"><br>
-Name <input type="text" id="namee" name="namee"><br>
-Branch <input type="text" id="branch" name="branch"><br>
-Marks <input type="text" id="marks" name="marks"><br>
-<input type="submit" value="submit">
-</form>
-</body>
+    <head>
+        Employee Database
+    </head>
+    <body>
+        <form action="insert" method="POST">
+            ID:  <input type="text" name="id" /><br/>
+            Name: <input type="text" name="name" /><br/>
+            Surname: <input type="text" name="surname" /><br/>
+            Designation: <input type="text" name="des" /><br/>
+            Salary: <input type="text" name="salary" /><br/>
+            <input type="submit" value="Submit">
+        </form>
+    </body>
 </html>
