c4bb920c58e4dbc782c60623c70e60b4bc7e9dee
Zubin Jimmy Paymaster <zubinpaymaster@yahoo.co.in>
2019-05-29 14:53:24 +0530

    Update 7b.js

--- 7b.js
+++ 7b.js
@@ -1,85 +1,98 @@
 var express=require('express')
-var mongoclient=require('mongodb').MongoClient
-var bodyparser=require('body-parser')
 var app=express()
-var urlencoded=bodyparser.urlencoded({extended:false})
-app.use(bodyparser.json())
-app.use(express.static('public'));
+var body=require('body-parser')
+var MongoClient=require('mongodb').MongoClient
+app.use(body.urlencoded({extended:false}))
+app.use(body.json())
 
-mongoclient.connect("mongodb://127.0.0.1/student",function(err,db){
-	if(err){
-		console.log(err);
+MongoClient.connect('mongodb://127.0.0.1/students',function(err,db)
+{
+	if(!err)
+	{
+		app.use(express.static('public'))
+		console.log('successfully Connected to db')
+		app.get('/',function(req,res)
+		{
+			res.sendFile(__dirname+'/public/index7.html')
+		})
+		app.get('/insert',function(req,res)
+		{
+			res.sendFile(__dirname+'/public/insert7.html')
+		})
+		app.listen(5000)
+		app.post('/process_post',function(req,res)
+		{
+			console.log(req.body)
+			res.setHeader('Content-Type', 'text/html')
+			console.log('Inside process')
+			var obj={'name':req.body.stuname,'usn':req.body.stuusn,'grade':req.body.grade}
+			db.collection('student_7b').insertOne(obj,function(req,res)
+			{
+				console.log('successfully Inserted')
+			})
+			res.end('successful insertion -->'+JSON.stringify(req.body))
+		})
+		app.get('/display',function(req,res)
+		{
+			db.collection('student_7b').find().toArray(function(err,i)
+			{
+				if(!err)
+				{
+					res.render('disp.ejs',{students:i})
+				}
+			})
+		})
 	}
-    app.get('/',function(req,res){
-        res.sendFile(__dirname+"/public/"+"index7.html")
-    });
-    app.get('/insert',(req,res)=>{
-        res.sendFile(__dirname+"/public/"+"insert7.html")
-    });
-    
-    app.post('/process_post',function(req,res){
-        console.log('process')
-        req.body.Message="Sending Data";
-        console.log(req.body);
-        var grade=req.body.grade
-        var name=req.body.name
-        db.collection('s').insert({name:name,grade:grade});
-        res.send("Student Data Record --->  "+JSON.stringify(req.body))
-    });
-    app.get('/display',(req,res)=>{
-        db.collection('s').find({grade:'S'}).toArray(function(err,data){
-                data.forEach(element => {
-                    res.send(element);
-                });
-        })
-    })
-    app.listen(5000);
-});
+})
 
 //public/index7.html
 <html>
-    <head>
-        <script src="angular/angular.min.js"></script>
-    </head>
-    <body ng-app="myapp">
-        <h1>Exam Management System</h1> <br/>
-        <a href="/insert">Insert</a><br/>
-        <a href="/display">Display</a><br/>
-    </body>
+<head>
+	<h3>EXAM SYSTEM</h3>
+	<script src='public/angular/angular.min.js'></script>
+</head>
+<body ng-app=''>
+	<a href='insert'>Insert</a></br>
+	<a href='display'>Display</a></br>
+</body>
 </html>
+
 
 //public/insert7.html
 <html>
-    <head>
-        <script src="angular/angular.min.js"></script>
-    </head>
-    <body ng-app="myapp">
-        <h1>Student Detail Entry</h1> <br/>
-        <form ng-controller="appcontroller" ng-submit="send()">
-            <h3>Name: </h3><input type="text"  ng-model="data.name"/><br/>
-            <h3>Grade: </h3><input type="text"  pattern="[ABCS]" ng-model="data.grade"/><br/>
-            <input type="submit"/>
-            <div>{{response.data}}</div>
-        </form>
-        <a href="/">Go back</a>
-        <script>
-            var info=angular.module("myapp",[])
-            info.controller("appcontroller",($scope,$http)=>{
-                $scope.data={}
-                $scope.response={}
-                $scope.send=function(){
-                    var posting=$http({
-                        method:'POST',
-                        url:'/process_post',
-                        data:$scope.data,
-                    }).then((response)=>{
-                        $scope.response.data=response.data;
-                    },
-                    (error)=>{
-                        console.log(error);
-                    });
-                }
-            });
-        </script>
-    </body>
+<head>
+	<script src='public/angular/angular.min.js'></script>
+	<script>
+	var myModule=angular.module("myapp",[])
+	myModule.controller('mycontroller',function($scope,$http)
+	{
+		$scope.data={}
+		$scope.response={}
+		$scope.send=function()
+		{
+			var posting=$http({
+				method:"POST",
+				url:"/process_post",
+				data:$scope.data
+			}).then(function(response)
+			{
+				console.log(response)
+				$scope.response.data=response.data
+			},function(error)
+			{
+				console.log(error)
+			})
+		}
+	})
+	</script>
+</head>
+<body ng-app="myapp" ng-controller="mycontroller">
+	<form ng-submit="send()">
+		Student Name<input type="text" ng-model="data.stuname"/><br>
+		Student USN<input type="text" ng-model="data.stuusn"/><br>
+		Grade<input type="text" ng-model="data.grade"/><br>
+		<input type="submit">
+		<div><pre>{{response.data}}<pre></div>
+	</form>
+</body>
 </html>
